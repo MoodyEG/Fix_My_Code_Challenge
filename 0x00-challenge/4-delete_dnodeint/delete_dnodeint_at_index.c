4e35ac2138b76605f7acba00e1cb4254271bcdf2
MoodyEG <moodyeg2011@yahoo.com>
2024-03-19 02:14:19 -0700

    Testing unmentioned cases

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -41,13 +41,11 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	else
 	{
-		(*head)->next->prev = (*head)->prev;
-		(*head)->prev->next = (*head)->next;
-		free(*head);
 		if ((*head)->next)
 			(*head)->next->prev = (*head)->prev;
-		if  ((*head)->prev)
+		if ((*head)->prev)
 			(*head)->prev->next = (*head)->next;
+		free(*head);
 		*head = saved_head;
 	}
 	return (1);
